82ce357aef1f9b2a70f36082401ceade251bd17c
rachier-alal <rachiermark285@gmail.com>
2022-12-28 06:28:13 +0300

    tabs

---
+++ kivy/numbers/main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+
+
+class Cops_db(Widget):
+    pass
+
+
+class CopsApp(App):
+    def build(self):
+        pass
+
+
+if __name__ == '__main__':
+    CopsApp().run()
